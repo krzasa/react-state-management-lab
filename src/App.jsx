e077f171c4b4d70e37b1f39feb774d93a2810e78
krzasa <krzasa96@gmail.com>
2024-05-13 09:14:20 -0400

    Forgot to add alert for not enough money

--- src/App.jsx
+++ src/App.jsx
@@ -84,10 +84,14 @@ const App = () => {
   const [totalStrength, setTotalStrength] = useState(0);
 
   const handleMove = (char) => {
+    if (char.price > money) {
     setTeam([...team, char]); // Add selected fighter to team array
     setMoney(money - char.price); // Update money
     setTotalAgility(totalAgility + char.agility); // Update total agility
     setTotalStrength(totalStrength + char.strength); // Update total strength
+  
+  } else {
+    alert('Not enough money');
   }
   const handleRemove = (index) => {
     const removedFighter = team[index];
